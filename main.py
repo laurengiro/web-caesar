1dcb34ccb1454a98524dd71afc1df752668d523f
Lauren Girolmetti <laurengiro@gmail.com>
2019-09-18 21:55:39 -0400

    Finish web Caesar

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -31,7 +31,7 @@ form = """
             </label>
 
             <label>
-                <textarea name="text"></textarea>
+                <textarea name="text">{0}</textarea>
             </label>
 
             <input type="submit" />
@@ -40,14 +40,14 @@ form = """
 </html>
 """
 
-@app.route("/", methods=['POST'])
+@app.route("/")
 def index():
-    return form
+    return form.format('')
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = int(request.form['rot'])
     text = request.form['text']
-    return '<h1>' + rotate_string(text, rot) + '</h1>'
+    return form.format(rotate_string(text, rot))
 
 app.run()
